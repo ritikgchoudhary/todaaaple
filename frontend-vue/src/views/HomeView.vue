4ead77f77fe1189ba17b9f8b700a73526f8e3c1b
root <root@srv1098512.hstgr.cloud>
2026-03-08 05:40:01 +0000

    auto-update: sync changes 2026-03-08 05:40:01

--- frontend-vue/src/views/HomeView.vue
+++ frontend-vue/src/views/HomeView.vue
@@ -19,10 +19,6 @@
           </router-link>
         </template>
         <template v-else>
-          <div class="headerBalance" @click="$router.push('/account')">
-            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="headerBalIcon"><circle cx="12" cy="12" r="10"/><path d="M16 8h-6a2 2 0 1 0 0 4h4a2 2 0 1 1 0 4H8"/><path d="M12 18V6"/></svg>
-            <span class="text">₹ {{ (userBalance || 0).toFixed(2) }}</span>
-          </div>
           <button class="btnLogout" @click="handleLogout" title="Logout">
             <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2.5" stroke-linecap="round" stroke-linejoin="round"><path d="M9 21H5a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h4"/><polyline points="16 17 21 12 16 7"/><line x1="21" y1="12" x2="9" y2="12"/></svg>
           </button>
