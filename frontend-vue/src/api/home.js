c709708be6b35b220f0651a423bad950b8fb8b92
root <root@srv1098512.hstgr.cloud>
2026-03-08 06:24:01 +0000

    auto-update: sync changes 2026-03-08 06:24:01

--- frontend-vue/src/api/home.js
+++ frontend-vue/src/api/home.js
@@ -4,3 +4,6 @@ export const getCarousel = () => API.get('/carousel')
 export const getNotice = () => API.get('/getNotice')
 export const getProviders = () => API.get('/getProviders')
 export const launchGame = (userId, gameId) => API.post(`/game/launch/${userId}`, { game_uid: gameId })
+
+export const getHomeCategoryGames = () => API.get('/admin-api/home-category-games')
+export const updateHomeCategoryGames = (data) => API.post('/admin-api/home-category-games', data)
