220b2579557adb1889a19b8065901811afe5cea1
root <root@srv1098512.hstgr.cloud>
2026-03-13 14:56:01 +0000

    auto-update: sync changes 2026-03-13 14:56:01

--- frontend-vue/src/stores/auth.js
+++ frontend-vue/src/stores/auth.js
@@ -1,5 +1,6 @@
 import { defineStore } from 'pinia'
 import { ref, computed } from 'vue'
+import * as walletApi from '../api/wallet'
 
 export const useAuthStore = defineStore('auth', () => {
   const token = ref(localStorage.getItem('auth_token') || '')
