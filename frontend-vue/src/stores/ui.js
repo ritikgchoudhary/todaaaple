ec54223e6ca6b6e35c460448778a9644e779a938
root <root@srv1098512.hstgr.cloud>
2026-03-13 16:58:01 +0000

    auto-update: sync changes 2026-03-13 16:58:01

--- frontend-vue/src/stores/ui.js
+++ frontend-vue/src/stores/ui.js
@@ -3,6 +3,7 @@ import { ref } from 'vue'
 
 export const useUiStore = defineStore('ui', () => {
   const isBottomNavVisible = ref(true)
+  const installPrompt = ref(null)
 
   function showBottomNav() {
     isBottomNavVisible.value = true
@@ -12,5 +13,19 @@ export const useUiStore = defineStore('ui', () => {
     isBottomNavVisible.value = false
   }
 
-  return { isBottomNavVisible, showBottomNav, hideBottomNav }
+  function setInstallPrompt(prompt) {
+    installPrompt.value = prompt
+  }
+
+  async function triggerInstallPrompt() {
+    if (installPrompt.value) {
+      installPrompt.value.prompt()
+      const { outcome } = await installPrompt.value.userChoice
+      if (outcome === 'accepted') {
+        installPrompt.value = null
+      }
+    }
+  }
+
+  return { isBottomNavVisible, showBottomNav, hideBottomNav, installPrompt, setInstallPrompt, triggerInstallPrompt }
 })
