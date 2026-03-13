2e8f0a10ce0801da45bc4989d472dc5f13d1d18c
root <root@srv1098512.hstgr.cloud>
2026-03-13 16:04:01 +0000

    auto-update: sync changes 2026-03-13 16:04:01

---
+++ backend/test_api2.js
@@ -0,0 +1,21 @@
+import axios from 'axios';
+
+async function testApi() {
+  try {
+    const loginRes = await axios.post('http://localhost:4001/signin', {
+      phone: '9988776655',
+      password: 'bypass'
+    });
+    const token = loginRes.data.token;
+    
+    // Now call getUserHome
+    const homeRes = await axios.get('http://localhost:4001/getUserHome/75645', {
+      headers: { Authorization: 'Bearer ' + token }
+    });
+    console.log("getUserHome response:");
+    console.log(JSON.stringify(homeRes.data, null, 2));
+  } catch (err) {
+    console.error("Error:", err.response ? err.response.data : err.message);
+  }
+}
+testApi();
