df2692f3729f49cc1d717af35c61db00c2ecc885
root <root@srv1098512.hstgr.cloud>
2026-03-14 18:00:01 +0000

    auto-update: sync changes 2026-03-14 18:00:01

--- frontend-vue/src/views/HomeView.vue
+++ frontend-vue/src/views/HomeView.vue
@@ -327,19 +327,6 @@
           <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><polyline points="15 18 9 12 15 6"></polyline></svg>
           Back
         </button>
-        
-        <div v-if="auth.isLoggedIn" class="headerBalance">
-          <div class="hBalTop">
-            <svg xmlns="http://www.w3.org/2000/svg" width="12" height="12" viewBox="0 0 24 24" fill="#fbbf24" stroke="#fbbf24" stroke-width="2"><circle cx="12" cy="12" r="10"/></svg>
-            <span>Balance</span>
-          </div>
-          <div class="hBalBottom">
-             <span class="hBalVal">₹{{ (userBalance || 0).toFixed(2) }}</span>
-             <button class="hRefreshBtn" @click="fetchBalance" :class="{ 'spinning': isRefreshing }">
-               <svg xmlns="http://www.w3.org/2000/svg" width="12" height="12" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2.5" stroke-linecap="round" stroke-linejoin="round"><path d="M3 12a9 9 0 1 0 9-9 9.75 9.75 0 0 0-6.74 2.74L3 8"/><path d="M3 3v5h5"/></svg>
-             </button>
-          </div>
-        </div>
       </div>
       <iframe :src="iframeUrl" frameborder="0" allowfullscreen></iframe>
     </div>
--- frontend-vue/src/views/LuckySportsView.vue
+++ frontend-vue/src/views/LuckySportsView.vue
@@ -7,7 +7,7 @@
           <img :src="siteLogoUrl || 'data:image/gif;base64,R0lGODlhAQABAIAAAAAAAP///yH5BAEAAAAALAAAAAABAAEAAAIBRAA7'" alt="Site" class="headerLogo" />
         </router-link>
         
-        <div v-if="auth.isLoggedIn" class="headerBalance">
+        <div v-if="auth.isLoggedIn && !iframeUrl" class="headerBalance">
           <div class="hBalTop">
             <svg xmlns="http://www.w3.org/2000/svg" width="12" height="12" viewBox="0 0 24 24" fill="#fbbf24" stroke="#fbbf24" stroke-width="2"><circle cx="12" cy="12" r="10"/></svg>
             <span>Balance</span>
