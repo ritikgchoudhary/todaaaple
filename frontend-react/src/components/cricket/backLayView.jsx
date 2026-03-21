1b1a75e4463ede457b16031c830eddb12c52c7fb
root <root@srv1098512.hstgr.cloud>
2026-03-21 15:36:02 +0000

    auto-update: sync changes 2026-03-21 15:36:02

--- frontend-react/src/components/cricket/backLayView.jsx
+++
@@ -1,8 +0,0 @@
-import { Container, Grid, Typography } from "@material-ui/core/";
-import React from 'react'
-
-export const BackLay = (props) => {
-    return (
-        <></>
-    )
-}
--- frontend-react/src/reducers/index.js
+++
@@ -1,4 +0,0 @@
-import { combineReducers } from 'redux';
-import Auth from './auth';
-
-export const reducers = combineReducers({Auth});
